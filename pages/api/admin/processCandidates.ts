4703efc283aa081f1d3e94d6eda67217c0e64ee6
Ayushdevhac <Ayushnhdc@gmail.com>
2025-06-20 15:32:29 +0530

    just correct the path

--- pages/api/admin/processCandidates.ts
+++ pages/api/admin/processCandidates.ts
@@ -1,3 +1,4 @@
+
 import type { NextApiRequest, NextApiResponse } from "next";
 import axios from "axios";
 import dbConnect from "../../../lib/mongodb";
@@ -59,12 +60,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
       )
       .send({ from: admin , gas: 1000000});
 
-    if(await voting.methods.startElection(3).send({ from: admin, gas: 1000000 })){
-      console.log("Voting started successfully");
-      console.log("start time:", await voting.methods.votingStart().call());
-      console.log("end time:", await voting.methods.votingEnd().call());
-      console.log("Election started successfully");
-    }
+    await voting.methods.startElection(5).send({ from: admin });
 
     for (const candidate of candidates) {
       console.log("Candidate from IPFS:", candidate);
